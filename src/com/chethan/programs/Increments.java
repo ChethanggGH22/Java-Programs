8c1596f9c678d81bb88f0d1fe6c7a46fc16f0794
Chethan Gollarahalli <chethan.gollarahalli@infor.com>
2026-02-14 12:27:45 +0530

    first commit

---
+++ src/com/chethan/programs/Increments.java
@@ -0,0 +1,23 @@
+package com.chethan.programs;
+
+public class Increments 
+{
+	public static void main(String[] args)
+	{
+		int a=100,b=100;
+//		System.out.println(a++);
+//		System.out.println(b--);
+		a++;
+		System.out.println(a);
+		a--;
+		System.out.println(a);
+//		int r1=a++;
+//		System.out.println(r1);
+//		System.out.println(a);
+//		int r2=b--;
+//		System.out.println(r2);
+//		System.out.println(b);
+		
+	}
+
+}
---
+++ src/com/chethan/programs/whileLoop.java
@@ -0,0 +1,19 @@
+package com.chethan.programs;
+
+public class whileLoop 
+{
+	public static void main(String[] args)
+	{
+		int count=1;
+		while(true)
+		{
+			System.out.println("Hello");
+			count++;
+			
+		if(count==11)
+		{
+			break;
+		}
+		}
+	}
+}
